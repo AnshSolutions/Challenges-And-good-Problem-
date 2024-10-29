4edc6118896f559569e538985e53e43bc328e739
AnshSolutions <157683511+AnshSolutions@users.noreply.github.com>
2024-10-29 08:27:21 +0530

    Update 2684.cpp

--- 2684.cpp
+++ 2684.cpp
@@ -33,3 +33,40 @@ int main()
     cout<<ans-1<<endl;
    }
 }
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(vector<vector<int>>& grid, int r, int c, int currcol, int currrow, int prev) {
+        if (currrow < 0 || currrow >= r || currcol >= c || grid[currrow][currcol] <= prev) {
+            return 0;
+        }
+        if (dp[currrow][currcol] != -1) {
+            return dp[currrow][currcol];
+        }
+
+        int a1 = solve(grid, r, c, currcol + 1, currrow - 1, grid[currrow][currcol]);
+        int a2 = solve(grid, r, c, currcol + 1, currrow + 1, grid[currrow][currcol]);
+        int a3 = solve(grid, r, c, currcol + 1, currrow, grid[currrow][currcol]);
+        
+        return dp[currrow][currcol] = 1 + max({a1, a2, a3});
+    }
+
+    int maxMoves(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        dp.assign(r, vector<int>(c, -1));
+
+        int maxi = INT_MIN;
+        for (int i = 0; i < r; i++) {
+            maxi = max(maxi, solve(grid, r, c, 0, i, INT_MIN));
+        }
+        return maxi - 1;
+    }
+};
